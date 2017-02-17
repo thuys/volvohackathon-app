c2d63cd806b194371e26c6d6d0d2406687edb7ac
Thomas Uyttendaele <thomas.uyttendaele@gmail.com>
2017-02-17 19:56:25 +0100

    Update

--- VolvoHackathon-App/src/com/deloitte/classes/datamodel/Truck.java
+++ VolvoHackathon-App/src/com/deloitte/classes/datamodel/Truck.java
@@ -2,6 +2,7 @@ package com.deloitte.classes.datamodel;
 
 public class Truck {
 	public String id = "ZER";
+	public ColourCode status = ColourCode.RED;
 	public Position position = new Position();
 	public TruckInformation information = new TruckInformation();
 
