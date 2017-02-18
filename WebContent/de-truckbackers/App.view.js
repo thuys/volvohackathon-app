70cfa091602cea5a76799e94124691c37e3de9a2
Patrick Reible <preible@deloitte.de>
2017-02-18 08:51:22 +0100

    push

--- WebContent/de-truckbackers/App.view.js
+++ WebContent/de-truckbackers/App.view.js
@@ -17,89 +17,85 @@ sap.ui.jsview("de-truckbackers.App", {
  		return new sap.m.Page({
  			showHeader: false,
 			content: [
-				new sap.m.HBox({
+				new sap.m.VBox({
 					items: [
-						new sap.m.VBox({
+						new sap.m.HBox({
+							justifyContent: "Center",
+							width: "100%",
 							items: [
-								new sap.m.HBox({
-									justifyContent: "Center",
-									width: "100%",
-									items: [
-										new sap.ui.core.HTML({
-											content: '<div id="header"></div>'
+								new sap.ui.core.HTML({
+									content: '<div id="header"></div>'
+								})
+							]
+						}),
+						new sap.m.IconTabBar({
+							id: "appTabBar",
+							items: [
+								new sap.m.IconTabFilter({
+									text: "Fleet Overview",
+									key: "fleetOverview",
+									content: [
+										new sap.m.VBox({
+											width: "100%",
+											items: [
+												new sap.m.Panel({
+													width: "100%",
+													height: "600px",
+													content: [
+														new sap.ui.core.HTML({
+															content: '<div id="map"></div>'
+														})
+													]
+												}),
+												new sap.m.Panel({
+													id: "dashboardPanel"
+												})
+											]
 										})
 									]
 								}),
-								new sap.m.IconTabBar({
-									id: "appTabBar",
-									items: [
-										new sap.m.IconTabFilter({
-											text: "Fleet Overview",
-											key: "fleetOverview",
-											content: [
-												new sap.m.VBox({
+								new sap.m.IconTabFilter({
+									text: "Alert Management",
+									key: "alertManagement",
+									id: "alertManagement",
+									enabled: false,
+									content: [
+										new sap.m.VBox({
+											width: "100%",
+											items: [
+												new sap.m.Panel({
 													width: "100%",
-													items: [
-														new sap.m.Panel({
-															width: "100%",
-															height: "600px",
-															content: [
-																new sap.ui.core.HTML({
-																	content: '<div id="map"></div>'
-																})
-															]
-														}),
-														new sap.m.Panel({
-															id: "dashboardPanel"
+													height: "400px",
+													content: [
+														new sap.ui.core.HTML({
+															content: '<div id="alertMap"></div>'
 														})
 													]
+												}),
+												new sap.m.Panel({
+													id: "alertPanel",
 												})
 											]
-										}),
-										new sap.m.IconTabFilter({
-											text: "Alert Management",
-											key: "alertManagement",
-											id: "alertManagement",
-											enabled: false,
-											content: [
-												new sap.m.VBox({
-													width: "100%",
-													items: [
-														new sap.m.Panel({
-															width: "100%",
-															height: "400px",
+										})
+									]
+								}),
+								new sap.m.IconTabFilter({
+									text: "Reports",
+									content: [
+										new sap.m.VBox({
+											width:"100%",
+											items:[
+												new sap.m.Panel({
+													headerToolbar : [
+														new sap.m.Toolbar({
 															content: [
-																new sap.ui.core.HTML({
-																	content: '<div id="alertMap"></div>'
-																})
+																new sap.m.Title({text:'Alert density map'})
 															]
-														}),
-														new sap.m.Panel({
-															id: "alertPanel",
-														})
-													]
-												})
-											]
-										}),
-										new sap.m.IconTabFilter({
-											text: "Reports",
-											content: [
-												new sap.m.VBox({
-													width:"100%",
-													items:[
-														new sap.m.Panel({
-															headerToolbar : [
-																new sap.m.Toolbar({
-																	content: [
-																		new sap.m.Title({text:'Alert density map'})
-																	]
-																})																
-															],
-															content:[
-																new sap.ui.core.HTML({
-																	content: '<iframe width="100%" height="600px" src="http://www.openheatmap.com/embed.html?map=PerloffTanseyUnquarrelled" ></iframe>'
-																})
-															]
+														})																
+													],
+													content:[
+														new sap.ui.core.HTML({
+															content: '<iframe width="100%" height="600px" src="http://www.openheatmap.com/embed.html?map=PerloffTanseyUnquarrelled" ></iframe>'
 														})
 													]
 												})
@@ -110,7 +106,7 @@ sap.ui.jsview("de-truckbackers.App", {
 							]
 						})
 					]
-				}).addStyleClass("tileStyleSingle")
+				})
 			]
  		})
 	}
